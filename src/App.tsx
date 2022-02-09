a1b21c1b7e4293f6e2697776f3b9f0b5b060e261
Allan <matrixboston7613@gmail.com>
2022-02-09 19:40:21 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
-function App() {
+
+
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <form action="">
+      <input name="Email"type="text" placeholder="Enter Email" />
+      <label htmlFor="Email"><b>Email</b></label>
+
+      <input name="Password"type="text" placeholder="Enter password" />
+      <label htmlFor="Password"><b>Password</b></label>
+     </form>
+     <button>Login</button>
+     
+   </div>
   );
 }
 
 export default App;
+
+/* 
+1. Change title in public/index.html
+2. Rename src/App.js => App.jsx
+3. Clear out code from return in App.js
+4. Clear out code from App.css
+5. Add global reset to index.css
+6. Install sass
+7. Rename App.css => App.module.scss & change import
+*/
