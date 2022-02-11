ccf6f8d33f01ba5240229aac4491f5579e1f4f72
Allan <matrixboston7613@gmail.com>
2022-02-11 14:43:04 +0800

    primary login form styling complete

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,17 +3,13 @@ import styles from "./LoginForm.module.scss";
 
 const LoginForm = () => {
   return (
-    <form className={styles.content} action="">
-      <h2>Login</h2>
-      <div className={styles.inputContainer}>
-        <label htmlFor="Email">Email</label>
-        <input name="Email" type="text" placeholder="Enter Email" />
-      </div>
-
-      <div className={styles.inputContainer}>
-        <label htmlFor="Password">Passwords</label>
-        <input name="Password" type="text" placeholder="Enter password" />
-      </div>
+    <form className={styles.box} action="">
+      <h1>Login</h1>
+      <input className={styles.text} type="text" placeholder="Email" />
+      <p>Please enter an email address</p>
+      <input className={styles.text} type="text" placeholder="Password" />
+      <p>Please enter a valid password </p>
+      <input className={styles.submit} type="submit" name="" id="" />
     </form>
   );
 };
