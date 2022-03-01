08cfecae826705a82ce574a21277077c8b63e595
Allan <matrixboston7613@gmail.com>
2022-03-01 18:11:41 +0800

    response from Orb server successfully detected; upon login

--- src/services/loginReg.service.ts
+++ src/services/loginReg.service.ts
@@ -12,5 +12,5 @@ export const deliverLogInData = async (dataObject: LoginData) => {
 
 export const verifyLogInData = async (dataObject: LoginData) => {
   const res = await axios.post("http://localhost:7777/login", dataObject);
-  console.log(res);
+  console.log(await res.data);
 };
