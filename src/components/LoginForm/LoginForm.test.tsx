7ba75d5d67770b6917a6a173bc726504e6aafcea
Allan <matrixboston7613@gmail.com>
2022-03-04 01:40:30 +0800

    general behaviour driven unit tests   for login form

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { shallow, ShallowWrapper } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import LoginForm from "./LoginForm";
 
-describe("SearchBar tests", () => {
-  let component: ShallowWrapper;
+describe("Login Form Tests", () => {
+  it("The form should render the corret fields", () => {
+    //1. Arrange
+    render(<LoginForm />);
 
-  beforeEach(() => {
-    component = shallow(<LoginForm />);
+    //2. Act
+    const emailInput = screen.getByPlaceholderText("Email");
+    const passwordInput = screen.getByPlaceholderText("Password");
+
+    //3. Assert
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
   });
 
-  it("should render", () => {
-    expect(component).toBeTruthy();
+  it("The form should detect when a field is not filled correctly", () => {
+    //1. Arrange
+    render(<LoginForm />);
+
+    //2.Act
+    const emailInput = screen.getByPlaceholderText("Email");
+    userEvent.type(emailInput, "memes");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    userEvent.click(screen.getByRole("button", { name: /Login/i }));
+
+    //3. Assert
+    const emailAlert = screen.queryByText(/Please enter an email address/i);
+    expect(emailAlert).toBeTruthy();
+    const passwordAlert = screen.queryByText(/Please enter a valid password/i);
+    expect(passwordAlert).toBeTruthy();
   });
 });
